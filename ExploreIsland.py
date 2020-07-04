1b899a68a8c1856a14fb7b925e545c9dc8b090f9
CapNCrunchy <65380841+CapNCrunchy@users.noreply.github.com>
2020-07-04 11:23:05 -0400

    Update ExploreIsland.py
    Changed frame refresh and changed the arrow keys fro WASD

--- ExploreIsland.py
+++ ExploreIsland.py
@@ -2,35 +2,38 @@
 import pygame
 
 pygame.init()
-background = pygame.image.load('Images\\IslandTest.jpg')
+background = pygame.image.load('.\\Images\\IslandTest.jpg')
+tutorial_font = (pygame.font.Font('HighTide-Demo.ttf',40)).render('Press \'x\' to leave', True, (0,0,0))
+
 def IslandExploration():
     screen = pygame.display.set_mode([800,555])
-    pygame.display.set_caption("Dungeon Crawl")
-    screen.blit(background,(0,0))
+    pygame.display.set_caption("Island Sample")
+    
     running = True
     x = 0
     y = 0
-    speed = 20
+    speed = 1
     while running:
-        pygame.time.delay(100)
+        pygame.time.delay(1)
+        screen.blit(background,(0,0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         keys = pygame.key.get_pressed()
             
-        if keys[pygame.K_LEFT] and x != 0:
+        if keys[pygame.K_a] and x != 0:
             x -= speed
-        if keys[pygame.K_RIGHT] and x != 545:
+        if keys[pygame.K_d] and x != 790:
             x += speed
-        if keys[pygame.K_UP] and y != 0:
+        if keys[pygame.K_w] and y != 0:
             y -= speed
-        if keys[pygame.K_DOWN] and y != 545:
+        if keys[pygame.K_s] and y != 790:
             y += speed
         if keys[pygame.K_x]:
             running = False
         
 
         pygame.draw.rect(screen, (255, 0, 0), (x,y,10,10), 5)
-
+        screen.blit(tutorial_font, (x,y+15,(tutorial_font.get_rect()).width,(tutorial_font.get_rect()).height))
         pygame.display.flip()
     screen = pygame.display.set_mode([1000,1000])
